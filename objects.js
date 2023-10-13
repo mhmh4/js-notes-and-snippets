e7c8b5539c1f7d6b697393e5e260c2f31df535ad
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-10-12 23:11:33 -0400

    Update objects.js

--- objects.js
+++ objects.js
@@ -1,16 +1,16 @@
-// Of the eight data types in JS, seven are primitive because their
-// values contain a single thing. Objects are used to store keyed
-// collections of various data and more complex entities.
+// Of the eight data types in JS, seven are primitive because their values
+// contain a single thing. Objects are used to store keyed collections of
+// various data and more complex entities.
 //
-// Objects are used a lot in JS, so we must understand them first
-// before going in-depth anywhere else.
+// Objects are used a lot in JS, so we must understand them first before going
+// in-depth anywhere else.
 //
-// An object can be created with figure brackets {} with an optional list
-// of properties. A property is a "key: value" pair, where key is a string
-// (also called a "property name"), and value can be anything.
+// An object can be created with figure brackets {} with an optional list of
+// properties. A property is a "key: value" pair, where key is a string (also
+// called a "property name"), and value can be anything.
 
-let _user = Object();
-let __user = {};
+// let user = Object();
+// let user = {};
 
 let user = {
   name: "John", // key "name" stores value "John"
@@ -32,8 +32,8 @@ delete user.age;
 
 console.log(user);
 
-// Adding square brackets to an object literal property is known as a
-// computed property.
+// Adding square brackets to an object literal property is known as a computed
+// property.
 
 let fruit = "apple";
 
@@ -61,16 +61,16 @@ let obj = {
 console.log(obj["0"]); // test
 console.log(obj[0]); // test (same property)
 
-// Accessing a property that doesn't exist won't give an error,
-// it will return undefined.
+// Accessing a property that doesn't exist won't give an error, it will return
+// undefined.
 
 let x = { a: 1, b: 2 };
 console.log(x.c);
 
 // Use the in keyword to see if a property exists in an object.
 console.log("c" in user);
-// Use a quoted string for the target property since a variable
-// would use it's value
+// Use a quoted string for the target property since a variable would use it's
+// value
 let b = false;
 console.log(b in x);
 
