3049eb9ccbbaf9704be9f8ba6885e11a62cc9837
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-10-13 23:22:09 -0400

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -12,3 +12,30 @@ console.log(fruits.at(-2));
 // unshift(x) adds element to the beginning of the array
 //
 // note push and unshift can take multiple arguments
+
+// Arrays are just a special type of object. Recall that there are only eight
+// basic data types.
+
+// Since they're objects, arrays are stored as references.
+let a = [];
+let b = a; // doesn't create a copy
+
+// iterating through our array
+for (let fruit of fruits) {
+  console.log(fruit);
+}
+
+// An alternate way of creating arrays is using its constructor
+let primes = new Array(2, 3, 5, 7);
+
+// If the constructor is called with a single number, it creates an empty array
+// with a length equal to the argument.
+
+let row = new Array(8);
+console.log(row); // [ <8 empty items> ]
+
+console.log(fruits);
+// Array.toString returns a comma-separated list of elements
+console.log(fruits.toString());
+
+// Don't compare arrays using `==`.
