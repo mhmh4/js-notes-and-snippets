589744666c60a0ecee6392058d2c6b94153acf2c
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-10-13 21:50:46 -0400

    Create arrays.js

---
+++ arrays.js
@@ -0,0 +1,14 @@
+let fruits = ["Apple", "Avocado", "Cherry", "Orange", "Plum"];
+
+console.log(fruits.length);
+
+console.log(fruits.at(0));
+console.log(fruits.at(-1));
+console.log(fruits.at(-2));
+
+// push(x) adds x to the end of an array
+// pop() removes and returns last element of an array
+// shift() removed first element of array
+// unshift(x) adds element to the beginning of the array
+//
+// note push and unshift can take multiple arguments
